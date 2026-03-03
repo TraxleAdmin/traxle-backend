cbb80505c4d50e122cc99a81bbdfa979c2cf4f0a
Traxle Developer <admin@traxleapp.com>
2026-03-04 02:21:06 +0300

    fix: Firebase Base64 kimlik dogrulamasi entegre edildi

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as admin from 'firebase-admin'; // 🔥 FIREBASE ADMIN EKLENDİ
+import * as admin from 'firebase-admin';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,17 +11,22 @@ async function bootstrap() {
     credentials: true,
   });
 
-  // 🔥 FIREBASE TANRI MODU BAŞLATILIYOR (Sistemin her yerinden erişilebilir)
-  if (process.env.FIREBASE_PROJECT_ID) {
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        // .env'den gelen \n karakterlerini gerçek alt satıra çeviriyoruz
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      }),
-    });
-    console.log(`🔥 Firebase Admin SDK Başarıyla Bağlandı!`);
+  // 🔥 YENİ NESİL DEVOPS: Base64 Çözücü ile Firebase Tanrı Modu
+  if (process.env.FIREBASE_BASE64) {
+    try {
+      const serviceAccount = JSON.parse(
+        Buffer.from(process.env.FIREBASE_BASE64, 'base64').toString('utf8')
+      );
+
+      admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+      });
+      console.log(`🔥 Firebase Admin SDK (Base64) Başarıyla Bağlandı!`);
+    } catch (error) {
+      console.error(`❌ Firebase Başlatma Hatası (Base64 Çözülemedi):`, error);
+    }
+  } else {
+    console.warn(`⚠️ FIREBASE_BASE64 değişkeni bulunamadı. Firebase senkronizasyonu çalışmayacak.`);
   }
 
   const port = process.env.PORT || 3000;
